b2622f1ec83829f5dcf62af8f181430293d2399a
bergh026 <herman.berghuijs@wur.nl>
2026-01-29 17:04:53 +0100

    Example files uploaded.

---
+++ main_example_griddedweather.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+from pathlib import Path
+import pandas as pd
+from pcse.base import ParameterProvider
+from pcse.input import YAMLCropDataProvider, NASAPowerWeatherDataProvider
+from pcse.models import Wofost81_PP
+import yaml
+
+latitude = 51.98
+longitude = 5.66
+
+cwd = Path.cwd()
+agro_fp = cwd / "9_Wofost81_PP_agro.yaml"
+soil_fp = cwd / "9_Wofost81_PP_soil.yaml"
+site_fp = cwd / "9_Wofost81_PP_site.yaml"
+weather_fp = cwd / "9_weather.csv"
+
+output_dir = cwd / "output"
+fig_fp = output_dir / "output_sim_griddedweather.jpeg"
+
+with open(agro_fp, "r") as f:
+    agro_dict = yaml.safe_load(f.read())
+
+with open(soil_fp, "r") as f:
+    soil_dict = yaml.safe_load(f.read())
+
+with open(site_fp, "r") as f:
+    site_dict = yaml.safe_load(f.read())
+
+crop_dict = YAMLCropDataProvider(Wofost81_PP)
+weather_data = NASAPowerWeatherDataProvider(latitude, longitude)
+parameters = ParameterProvider(sitedata=site_dict, soildata=soil_dict, cropdata=crop_dict)
+wofost = Wofost81_PP(parameters, weather_data, agro_dict)
+
+wofost.run_till_terminate()
+output = wofost.get_output()
+df_output = pd.DataFrame(output)
+
+fig, ax = plt.subplots()
+ax.plot(df_output["day"], df_output["WSO"])
+fig.savefig(fig_fp)
